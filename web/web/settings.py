1366e0ed950e37dccae56a1569a0d0559434da26
ZoREfLy <zoreflyc@gmail.com>
2017-12-19 20:53:11 -0800

    change

--- web/web/settings.py
+++ web/web/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'News',
         'USER': 'postgres',
-        'PASSWORD': 'Csk@006026',
+        'PASSWORD': '&&&&&&&',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
